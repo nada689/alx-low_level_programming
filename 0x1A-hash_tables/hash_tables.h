c37c8ab9574b1ec6a8d12b509b16eba2c5b74656
nada689 <120287148+nada689@users.noreply.github.com>
2023-09-13 01:40:45 +0300

    hash_tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,17 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
 typedef struct hash_node_s
 {
 	char *key;
